9a91383bfac245e0c91cd69d19f513637f57ed8d
zcw <zcw@java.com>
2019-12-12 23:32:26 +0800

    第五次提交，这是redis各种数据类型使用范例

--- src/main/java/RedisTest.java
+++ src/main/java/RedisTest.java
@@ -4,6 +4,7 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.List;
+import java.util.Set;
 
 public class RedisTest {
     @Test
@@ -158,7 +159,48 @@ public class RedisTest {
         }
     }
 
+    @Test
+    public static void sortedSet() {
+        System.out.println("==SoretedSet==");
+        Jedis jedis = RedisUtil.getJedis();
+        try {
+            jedis.zadd("hackers", 1940, "Alan Kay");
+            jedis.zadd("hackers", 1953, "Richard Stallman");
+            jedis.zadd("hackers", 1965, "Yukihiro Matsumoto");
+            jedis.zadd("hackers", 1916, "Claude Shannon");
+            jedis.zadd("hackers", 1969, "Linus Torvalds");
+            jedis.zadd("hackers", 1912, "Alan Turing");
+            Set<String> setValues = jedis.zrange("hackers", 0, -1);
+            System.out.println(setValues);
+            Set<String> setValues2 = jedis.zrevrange("hackers", 0, -1);
+            System.out.println(setValues2);
 
+
+            // 清空数据
+            System.out.println(jedis.flushDB());
+            // 添加数据
+            jedis.zadd("zset", 10.1, "hello");
+            jedis.zadd("zset", 10.0, ":");
+            jedis.zadd("zset", 9.0, "zset");
+            jedis.zadd("zset", 11.0, "zset!");
+            // 元素个数
+            System.out.println(jedis.zcard("zset"));
+            // 元素下标
+            System.out.println(jedis.zscore("zset", "zset"));
+
+            // 集合子集
+            System.out.println(jedis.zrange("zset", 0, -1));
+            // 删除元素
+            System.out.println(jedis.zrem("zset", "zset!"));
+            System.out.println(jedis.zcount("zset", 9.5, 10.5));
+            // 整个集合值
+            System.out.println(jedis.zrange("zset", 0, -1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+//            RedisUtil.getPool().returnResource(jedis);
+        }
+    }
 
     @Test
     public void pool() {
